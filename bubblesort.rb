5947198554c6f107f9967a0a4072d328ca4c4bb2
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-14 00:56:54 -0700

    bubblesort completed, substrings not completed

---
+++ bubblesort.rb
@@ -0,0 +1,44 @@
+arr = [4,3,78,2,0,2]
+
+#this bubble-sorts an array
+def bubble_sort(arr)
+  flag = true
+  while flag == true
+    flag = false
+    (0..arr.length - 2).each do |i|
+  
+      if arr[i] > arr[i+1]
+	    arr[i],arr[i+1] = arr[i+1],arr[i]
+		flag = true
+	  end
+    end
+  end
+  puts arr.to_s
+	
+end
+
+puts bubble_sort(arr)
+
+def bubble_sort_by(arr)
+
+#this function can bubble sort an array of words by size 
+def bubble_sort_by(arr)
+  flag = true
+  while flag == true
+    flag = false
+    (0..arr.length - 2).each do |i|
+      if yield(arr[i],arr[i+1]) < 0
+        arr[i],arr[i+1]=arr[i+1],arr[i]
+		flag = true
+      end
+    end
+  end
+  arr
+end
+
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  right.length - left.length
+end
+
