3ab98438b95dcac39d5fa5499aa94888c51efdca
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-15 16:08:33 -0700

    added players

--- classes.rb
+++ classes.rb
@@ -8,8 +8,14 @@ class Warrior
   end
   
   def about
-    puts "${@name} is a Warrior from the #{@clan} clan. 
+    puts "#{@name} is a Warrior from the #{@clan} clan. 
 	Strength is: #{@strength} Dexterity is #{@dexterity}."
   end
   
 end
+
+player_1 = Warrior.new("Raze","WhiteFox",23,21)
+player_2 = Warrior.new("Bazerk","RedFang",19,31)
+
+player_1.about
+player_2.about
