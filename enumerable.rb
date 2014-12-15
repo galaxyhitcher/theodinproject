88d6d08219a29af7586cce6824fbf11f9e00b449
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-15 01:55:44 -0700

    added indentation

--- enumerable.rb
+++ enumerable.rb
@@ -1,99 +1,99 @@
 module Enumerable
 #first three methods are taken from Jamie's solution
 #https://github.com/Jberczel/odin-projects/blob/master/ruby_advanced/project2.rb
-def my_each
-  return self unless block_given?
-  for i in self
-    yield(i)
-  end
-end
-
-
-def my_each_with_index
-  
-  return self unless block_given?
-  for i in (0..length)
-    yield(i)
-  end
-end
-
-def my_select
-  return self unless block_given?
-  arr = []
-  self.my_each do |x|
-    if yield(x)
-	  arr.push(x)
+	def my_each
+	  return self unless block_given?
+	  for i in self
+		yield(i)
+	  end
 	end
-  end
-  arr
-end
-    
-def my_all?
-  return self unless block_given?
-  self.my_each do |x|
-    if !yield(x)
-	  return false
-	end
-  end
-  return true
-end
 
-def my_none?
-  return self unless block_given?
-  self.my_each do |x|
-    if yield(x)
-	  return false
-	end
-  end
-  return true
-end
 
-def my_count(n=nil)
-  count = 0
-  unless block_given?
-    if n != nil
+	def my_each_with_index
+	  
+	  return self unless block_given?
+	  for i in (0..length)
+		yield(i)
+	  end
+	end
+
+	def my_select
+	  return self unless block_given?
+	  arr = []
 	  self.my_each do |x|
-	    if x == n
+		if yield(x)
+		arr.push(x)
+	  end
+	  end
+	  arr
+	end
+		
+	def my_all?
+	  return self unless block_given?
+	  self.my_each do |x|
+		if !yield(x)
+		return false
+	  end
+	  end
+	  return true
+	end
+
+	def my_none?
+	  return self unless block_given?
+	  self.my_each do |x|
+		if yield(x)
+		return false
+	  end
+	  end
+	  return true
+	end
+
+	def my_count(n=nil)
+	  count = 0
+	  unless block_given?
+		if n != nil
+		self.my_each do |x|
+		  if x == n
 		  count += 1
 		end
+		end
+		return count
+	  else
+		return self.length
+	  end
+	  end
+	  self.my_each do |x|
+		if yield(x)
+		count += 1
+	  end
 	  end
 	  return count
-	else
-	  return self.length
 	end
-  end
-  self.my_each do |x|
-    if yield(x)
-	  count += 1
+
+	def my_map
+	  #this still needs editing to complete the exercise 
+	  return self unless block_given?
+	  self.my_each do |x|
+		x = yield(x)
+	  end
+	  return self
 	end
-  end
-  return count
-end
 
-def my_map
-  #this still needs editing to complete the exercise 
-  return self unless block_given?
-  self.my_each do |x|
-    x = yield(x)
-  end
-  return self
-end
-
-def my_inject(n=nil)
-  if n == nil
-    result = 0
-  else
-    result = n
-  end
-  if !block_given?
-    raise LocalJumpError
-  
-  end
-  self.my_each do |x|
-    result = yield(result,x)
-  end
-  return result
-end
+	def my_inject(n=nil)
+	  if n == nil
+		result = 0
+	  else
+		result = n
+	  end
+	  if !block_given?
+		raise LocalJumpError
+	  
+	  end
+	  self.my_each do |x|
+		result = yield(result,x)
+	  end
+	  return result
+	end
 
 
 end
@@ -104,4 +104,3 @@ def multiply_els(arr)
 end
 
 multiply_els([2,4,5])
-
