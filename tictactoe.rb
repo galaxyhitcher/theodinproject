71ede6525fe3551a1eef8b9305bb8b87904f6f88
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-15 21:40:06 -0700

    finished tictactoe.rb without error checking

--- tictactoe.rb
+++ tictactoe.rb
@@ -25,24 +25,23 @@ class Board
   
   def win?
     token = @board[0][0]
-	if (@board[0][0]==token) & (@board[0][1]==token) & (@board[0][2]==token)
-	  puts token + " YOU WON!"
-	elsif (@board[1][0]==token) & (@board[1][1]==token) & (@board[1][2]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[2][0]==token) & (@board[2][1]==token) & (@board[2][2]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[0][0]==token) & (@board[1][0]==token) & (@board[2][0]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[1][0]==token) & (@board[1][1]==token) & (@board[1][2]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[2][0]==token) & (@board[2][1]==token) & (@board[2][2]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[0][0]==token) & (@board[1][1]==token) & (@board[2][2]==token)
-	  puts token + "YOU WON!"
-	elsif (@board[0][2]==token) & (@board[1][1]==token) & (@board[2.][2]==token)
-	  puts token + "YOU WON!"
-	else
-	  puts "NO"
+	if (@board[0][0]==token) & (@board[0][1]==token) & (@board[0][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[1][0]==token) & (@board[1][1]==token) & (@board[1][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[2][0]==token) & (@board[2][1]==token) & (@board[2][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[0][0]==token) & (@board[1][0]==token) & (@board[2][0]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[1][0]==token) & (@board[1][1]==token) & (@board[1][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[2][0]==token) & (@board[2][1]==token) & (@board[2][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[0][0]==token) & (@board[1][1]==token) & (@board[2][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	elsif (@board[0][2]==token) & (@board[1][1]==token) & (@board[2][2]==token) & (token != " ")
+	  return token + " YOU WON!"
+	
 	end
   end
   
@@ -50,10 +49,71 @@ class Board
 end
 
 class Player
-  def initialize(sym)
+  def initialize(sym,name)
     @sym = sym
+	@name = name
   end
   def mark_board(board,row,col)
     board.change_board(@sym,row,col)
   end
+  
+  def name
+    return @name
+  end
+  
+  def sym
+    return @sym
+  end
 end
+
+puts "Who will be X?"
+p1 = gets.chomp
+player1 = Player.new('X',p1)
+
+puts "Who will be O?"
+p2 = gets.chomp
+player2 = Player.new('O',p2)
+puts "  "
+
+board = Board.new
+board.print_board
+puts "  "
+
+flip = 1
+
+puts player1.name + "'s turn"
+puts "  "
+
+while !board.win?
+  col = 0
+  row = 0
+  if flip == 1
+    flip *= -1
+    puts "Row?"
+	row = gets.chomp.to_i
+	puts "Column?"
+	col = gets.chomp.to_i
+	player1.mark_board(board,row,col)
+	puts "  "
+  else
+    flip *= -1
+	puts "Row?"
+	row = gets.chomp.to_i
+	puts "Column?"
+	col = gets.chomp.to_i
+	player2.mark_board(board,row,col)
+	puts "  "
+  end
+  puts "next turn"
+  puts "  "
+  puts board.print_board
+  if (flip == 1) & (!board.win?)
+	puts player1.name + "'s turn"
+	puts "  "
+  elsif (flip==1) & (!board.win?)
+    puts player2.name + "'s turn"
+	puts "  "
+  end
+end
+
+puts board.win?
