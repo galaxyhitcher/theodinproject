c6dc347681f49bb4addbe9db9d65f3e711a852aa
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-17 16:16:33 -0700

    started defining mastermind.rb classes

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,20 @@
+#eventually refactor Codemaker < Player and Codebreaker < Player
+
 #codemaker class
+class Codemaker
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+   puts @name
+  end
+end
+
 #codebreaker class
+class Codebreaker
+end
+
 #board class
+class Board
+end
