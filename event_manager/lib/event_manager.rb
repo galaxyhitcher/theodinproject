c5f4da0e76c5716af8554365ec0ac3572694700b
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-17 02:20:50 -0700

    finished exercise

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -2,35 +2,8 @@ require 'csv'
 require 'sunlight/congress'
 require 'erb'
 
+
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
-def clean_phone_number(number)
-  
-  #If the phone number is less than 10 digits assume that it is a bad number
-  if number.to_s.size < 10
-    return "Invalid number"
-  
-  #If the phone number is 10 digits assume that it is good
-  elsif number.to_s.size == 10
-    number = number.to_s.split('')
-	number.shift
-	return number.join('').to_i
-	
-  #If the phone number is 11 digits and the first number is 1, trim the 1 and use the first 10 digits
-  elsif number.to_s.size == 11
-    number = number.to_s.split('')
-	number.shift
-	return number.join('')
-  #If the phone number is 11 digits and the first number is not 1, then it is a bad number
-  elsif (number.to_s.size == 11) & (number.to_s.split('')[0] != "1")
-    return "Invalid number"
-	
-  #If the phone number is more than 11 digits assume that it is a bad number
-  elsif number.to_s.size > 11
-    return "Invalid number"
-  end
-end
-
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -52,20 +25,30 @@ end
 
 puts "EventManager initialized."
 
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents = CSV.open 'full_event_attendees.csv', headers: true, header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
+all_hours = Hash.new(0) 
+days_of_week = Hash.new(0)
 contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+  row_date = DateTime.strptime(row[:regdate],'%m/%d/%y %H:%M')
   
-  legislators = legislators_by_zipcode(zipcode)
-  
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letters(id,form_letter)
+  all_hours[row_date.hour] += 1
+  days_of_week[row_date.strftime('%A')] += 1
 end
 
+def best_times(ahash)
+  result = []
+  max = ahash.values.max
+  ahash.each do |key,value|
+    if value == max
+	  result.push(key)
+    end
+  end
+  result
+end
+
+puts "the busiest hour(s) are: " + best_times(all_hours).to_s
+puts "the busiest day(s) are: " + best_times(days_of_week).to_s
