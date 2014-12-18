c5a1ec5b1e2dbf3a856458a0db740e9b545ce031
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-17 21:10:57 -0700

    added iswon? method, changed print_board method

--- mastermind.rb
+++ mastermind.rb
@@ -24,24 +24,53 @@ class Codebreaker
   def name
    puts @name
   end
+  
+  def mark_board
+  end
+  
+  def prints_board
+    (1..12).each do |i|
+	puts decoding_board[i]
+	end
+  end
 end
 
 #board class
 class Board
-  colors = {"red"=>"R","orange"=>"O","yellow":"Y","green":"G","blue":"B","indigo":"I"}
-  #this is the decoding board
-  decoding_board = [[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "],
-					[" "," "," "," "]]
-  
+  def initialize
+    
+	@decoding_board = [["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"],
+					["O","O","O","O"]]
+  end
+ 
+  def print_board
+	(1..12).each do |i|
+	  result = []
+	  (0..3).each do |j|
+		result.push(@decoding_board[i][j])
+				
+		end
+		puts result.join(' ')
+	  end
+  end
+  def iswon?
+    to_match = @decoding_board[0]
+	(1..12).each do |i|
+	  if @decoding_board[i]==to_match
+	    return true
+	  end
+	end
+	return false
+  end
 end
