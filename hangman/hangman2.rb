a160258fd17e5dc5407eb4ae46fa0c4bccc766a2
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-23 21:10:57 -0700

    Added some pseudocode to hangman.rb

--- hangman/hangman2.rb
+++ hangman/hangman2.rb
@@ -1,10 +1,15 @@
 require 'yaml'
 
+#the idea is to have a Game object which can be serialized into YAML and then reloaded from YAML back to a game object
+
 class Game
 
   def initialize
     @wrong_guesses = 0
 	@current_state
+  end
+  
+  def load_game
   end
   
   def save_game
