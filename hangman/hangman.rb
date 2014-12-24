fd31a6ad59aded98e9018f23ea46626c51754c7b
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-24 00:06:05 -0700

    made significant progress on hangman2.rb.  Renamed hangman2.rb to hangman.rb, deleted hangman.rb.

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,68 +1,130 @@
-#need to make program case insensitive
-#basic functionality in place, but i need to get serializing and wrong guesses feature to work
-f = IO.readlines("5desk.txt")
+require 'yaml'
+
+#the idea is to have a Game object which can be serialized into YAML and then reloaded from YAML back to a game object
 
 
-#randomly select word between 5 and 12 characters long
-choices = Array.new
-f.each do |word|
-  if (word.size > 5) & (word.size < 12)
-    choices.push(word)
+class Game
+  @@games = Array.new
+  def initialize
+	@wrong_guesses = 0
+	@secret_word = get_word
+	@current = "_"*(@secret_word.size - 1)
+	@saved = nil
   end
-end
-
-secret_word = choices.sample
-
-current = "_"*(secret_word.length - 1)
-
-puts current
-flag = true
-wrong_guesses = 0
-while flag == true
-	puts "guess"
-
+  
+  def get_word
+    f = IO.readlines("5desk.txt")
+	choices = Array.new
+	f.each do |word|
+	  if (word.size > 5) & (word.size < 12)
+        choices.push(word)
+      end
+    end
+	choices.sample
+  end
+  
+  def games
+    @@games
+  end
+  
+  def wrong_guesses
+    @wrong_guesses
+  end
+  
+  def secret_word
+    @secret_word
+  end
+  
+  def current
+    @current
+  end
+  
+  def saved
+    @saved
+  end
+  
+  def load_game(obj)
+    temp = YAML.load(obj::saved)
+	@wrong_guesses = temp::wrong_guesses
+	@secret_word = temp::secret_word
+	@current = temp::current
+	@saved = temp::saved
+  end
+  
+  
+  def save_game
+    @saved = YAML.dump(self)
+	@@games.push(@saved)
+  end
+  
+  def get_guess
+    puts "your turn to guess"
 	guess = gets.chomp
-	
-	right_guess = false
-	(0..current.length).each do |i|
-	  if secret_word[i] == guess
-	    right_guess = true
+	current_changed = false
+	(0..@current.size).each do |i|
+	  if guess == @secret_word[i]
+	    @current[i] = guess
+		current_changed = true
 	  end
 	end
-	(0..current.length).each do |i|
-		  if secret_word[i]==guess
-			current[i] = guess
-			flag = false
-			break
-		  
-		  end
-			
+	if !current_changed & !(guess=="?") & !(guess == "end")
+	  @wrong_guesses += 1
 	end
-	
-	
-
-		(0..current.length).each do |i|
-		  if secret_word[i]==guess
-			current[i] = guess
-		  
-		  end
-			
-		end
-	
-	
-	flag = false
-	(0..current.length).each do |i|
-	  if current[i] == "_"
-	    flag = true
+	guess
+  end
+  
+  def won?
+    won = true
+	(0..current.size).each do |i|
+	  if current[i] == '_'
+	    won = false
 	  end
 	end
-    puts current
-	if !(right_guess)
-	  wrong_guesses += 1
-	  puts "wrong_guesses:"
+	won
+  end
+  
+  def display_win
+    puts "You won!"
+  end
+  
+  def play_game
+    
+    while !won?
+	  g1 = get_guess
+	  if g1 == "?"
+	    @saved = save_game
+	  elsif g1 == "end"
+	    break
+	  end
+	  puts current
 	  puts wrong_guesses
+	  
 	end
-	
+	if won?
+	  puts "You won!"
+	else
+	  puts "Game saved."
+	end
+  end
+  
+  
 end
 
-puts "You figured it out!"
+puts "press L to load a game or anything else to continue"
+
+g1 = Game.new
+
+g1.play_game
+
+puts g1::games
+
+g2 = Game.new
+
+g2.play_game
+
+puts g2::games
+
+
+
+
+
