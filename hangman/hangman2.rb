8a4bf6a6d9894b15c11875ab73fb2ea404bf82fe
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-23 20:42:21 -0700

    created hangman2.rb

---
+++ hangman/hangman2.rb
@@ -0,0 +1,20 @@
+require 'yaml'
+
+class Game
+
+  def initialize
+    @wrong_guesses = 0
+	@current_state
+  end
+  
+  def save_game
+  end
+  
+  def get_guess
+  end
+  
+  def display_win
+  end
+  
+  
+end
