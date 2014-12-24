a18a3a7bf5e75510b85ff5f39f69647f7b6a7d24
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2014-12-23 22:24:24 -0700

    added basic functionality to hangman2.rb

--- hangman/hangman2.rb
+++ hangman/hangman2.rb
@@ -4,9 +4,34 @@ require 'yaml'
 
 class Game
 
+  def get_word
+    f = IO.readlines("5desk.txt")
+	choices = Array.new
+	f.each do |word|
+	  if (word.size > 5) & (word.size < 12)
+        choices.push(word)
+      end
+    end
+	choices.sample
+  end
+
+  
   def initialize
     @wrong_guesses = 0
-	@current_state
+	@secret_word = get_word
+	@current = "_"*(@secret_word.size - 1)
+  end
+  
+  def wrong_guesses
+    @wrong_guesses
+  end
+  
+  def secret_word
+    @secret_word
+  end
+  
+  def current
+    @current
   end
   
   def load_game
@@ -16,10 +41,48 @@ class Game
   end
   
   def get_guess
+    puts "your turn to guess"
+	guess = gets.chomp
+	current_changed = false
+	(0..@current.size).each do |i|
+	  if guess == @secret_word[i]
+	    @current[i] = guess
+		current_changed = true
+	  end
+	end
+	if !current_changed
+	  @wrong_guesses += 1
+	end
+  end
+  
+  def won?
+    won = true
+	(0..current.size).each do |i|
+	  if current[i] == '_'
+	    won = false
+	  end
+	end
+	won
   end
   
   def display_win
+    puts "You won!"
+  end
+  
+  def play_game
+    while !won?
+	  get_guess
+	  puts current
+	  puts wrong_guesses
+	end
+	display_win
   end
   
   
 end
+
+g1 = Game.new
+
+g1.play_game
+
+
