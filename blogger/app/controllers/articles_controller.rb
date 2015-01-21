cbd03260c4459564a48109277ffeebfe4f4f7f4b
unknown <1961025@T-SMD1021424.SDSMT.local>
2015-01-21 01:35:58 -0700

    listing all tags feature not yet completed - finish this later

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -40,4 +40,5 @@ class ArticlesController < ApplicationController
 	  redirect_to article_path(@article)
   end
   
+  
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  
   Blogger::Application.routes.draw do
+    root to: 'articles#index'
     resources :articles do
 	  resources :comments
 	end
+	resources :tags
 	
   end
   # The priority is based upon order of creation: first created -> highest priority.
