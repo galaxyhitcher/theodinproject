f47113c79b30f94bccae7a51c2999108fd325874
unknown <1961025@T-SMD1021424.SDSMT.local>
2015-01-20 22:21:45 -0700

    form-based workflow feature completed

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221054325) do
+ActiveRecord::Schema.define(version: 20150120102417) do
 
-  create_table "articles", force: true do |t|
+  create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
