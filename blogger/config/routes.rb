e0cd723a5c8fd7efa3de6797483c225eb6af0781
unknown <1961025@T-SMD1021424.SDSMT.local>
2015-01-21 00:25:46 -0700

    finished blog comments feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   Blogger::Application.routes.draw do
-    resources :articles
+    resources :articles do
+	  resources :comments
+	end
+	
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
